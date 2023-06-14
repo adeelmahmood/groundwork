fbeea0071006a57df75cb1c1a569ac11b59fc0e9
aqureshiest <adeelqureshi@M6TWYY7lQureshi.lan>
2023-06-14 01:02:50 -0400

    trying redirect url from auth callback page

--- src/app/auth/callback/route.js
+++ src/app/auth/callback/route.js
@@ -5,6 +5,23 @@ import { NextResponse } from "next/server";
 export async function GET(request) {
     const requestUrl = new URL(request.url);
     const code = requestUrl.searchParams.get("code");
+    const redirectedFrom = searchParams.get("redirectedFrom");
+
+    const redirectURL = () => {
+        let url =
+            process?.env?.NEXT_PUBLIC_OAUTH_REDIRECT_URL ?? // Manually set in vercel (mainly for staging)
+            process?.env?.NEXT_PUBLIC_SITE_URL ?? // This points to prod url
+            "http://localhost:3000/"; // local
+        // Make sure to include `https://` when not localhost.
+        url = url.includes("http") ? url : `https://${url}`;
+        // Make sure to including trailing `/`.
+        url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+        // if redirect query is provided, add that too
+        if (redirectedFrom) {
+            url = `${url}login-redirect?redirectedFrom=${encodeURI(redirectedFrom)}`;
+        }
+        return url;
+    };
 
     if (code) {
         const supabase = createRouteHandlerClient({ cookies });
@@ -12,5 +29,5 @@ export async function GET(request) {
     }
 
     // URL to redirect to after sign in process completes
-    return NextResponse.redirect(requestUrl.origin);
+    return NextResponse.redirect(redirectURL());
 }
