b4b50c35a8628b8d02248797b77f10fbb643cf20
aqureshiest <adeelqureshi@M6TWYY7lQureshi.lan>
2023-06-28 13:50:55 -0400

    updated base urls for client

--- src/modules/clients/receptionist-client.ts
+++ src/modules/clients/receptionist-client.ts
@@ -1,7 +1,8 @@
 export class AiReceptionistClient {
-    private baseUrl: string = process.env.VERCEL_URL
-        ? "https://" + process.env.VERCEL_URL
-        : "http://localhost:3000";
+    private baseUrl: string =
+        process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL
+            ? "https://" + (process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL)
+            : "http://localhost:3000";
 
     constructor() {}
 
